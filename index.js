fde2868e9bfcb59913069dfd5e07a2329ebec773
Jauhar Goga <jauhargoga@gmail.com>
2026-03-26 23:23:46 +1300

    working search function but i need to remove output.css lol

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-const wordDict = ["and", "or", "not"]
+const wordDict = ["and", "or", "not"];
 
-function search(dict, word){
+function search(dict, word) {
   let res = [];
-  for (let i = 0; i < dict.length; i++){
-    if ((dict[i].include(word) && dict[i].length > word.length) || (dict[i] === word)){
-      res.push(dict[i])
+  for (let i = 0; i < dict.length; i++) {
+    if (
+      (dict[i].include(word) && dict[i].length > word.length) ||
+      dict[i] === word
+    ) {
+      res.push(dict[i]);
     }
   }
   return res;
 }
 
+const searchInput = document.querySelector("#searchInput"); 
+const searchButton = document.querySelector("#searchButton");
+const wordDisplay = document.querySelector("#wordDisplay");
 
+for (let i = 0; i < wordDict.length; i++) {
+  wordDisplay.innerHTML += `<p>${wordDict[i]}</p>`;
+}
 
-var searchInput; 
-const searchButton = document.querySelector("#searchButton"); 
-
-const wordDisplay = document.querySelector("#wordDisplay")
-
-// for (let i = 0; i < wordDict.length; i++){
-//   wordDisplay.innerHTML += `<p>${wordDict[i]}</p>`;
-// }
-
-
-
+searchButton.addEventListener("click", () => {
+  const query = searchInput.value.trim();
+  if (query) {
+    const res = search(wordDict, query);
+    wordDisplay.innerHTML = res.map((word) => `<p>${word}</p>`).join("");
+  } else {
+    wordDisplay.innerHTML = wordDict.map((word) => `<p>${word}</p>`).join("");
+  }
+});
